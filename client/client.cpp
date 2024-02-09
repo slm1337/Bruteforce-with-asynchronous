c753f2e74a925807209a89a9552da03f53249f68
slm1337 <e1337ajke@gmail.com>
2024-02-09 14:16:04 +0200

    Добавьте файлы проекта.

---
+++ client/client.cpp
@@ -0,0 +1,218 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <thread>
+#include <future>
+#include <limits>
+#include <chrono>
+#include <vector>
+
+class PasswordCracker {
+private:
+	const std::string alphabet0 = "abcdefghijklmnopqrstuvwxyz'";
+	const std::string alphabet1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'0123456789";
+	const std::string alphabet2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'0123456789абвгдежзийклмнопрстуфхцчшщъыьэю€јЅ¬√ƒ≈∆«»… ЋћЌќѕ–—“”‘’÷„ЎўЏџ№Ёёя";
+
+	std::vector<std::string> alphabets = { alphabet0, alphabet1, alphabet2 };
+
+	bool BruteForce(HANDLE hPipe, const std::string& login, int numberOfAlphabet, int passLength) {
+		const std::string alphabet = alphabets[numberOfAlphabet];
+
+		for (int length = 1; length <= passLength; ++length) {
+			std::string password(length, ' ');
+
+			while (true) {
+				int index = 0;
+				while (index < length && password[index] == alphabet.back()) {
+					password[index++] = alphabet.front();
+				}
+
+				if (index == length) {
+					break;
+				}
+
+				password[index] = alphabet[alphabet.find(password[index]) + 1];
+
+				std::string message = login + ":" + password;
+				OVERLAPPED writeOverlapped;
+				ZeroMemory(&writeOverlapped, sizeof(OVERLAPPED));
+				writeOverlapped.hEvent = CreateEvent(nullptr, TRUE, FALSE, nullptr);
+
+				if (WriteFile(hPipe, message.c_str(), message.length() + 1, nullptr, &writeOverlapped) || GetLastError() == ERROR_IO_PENDING) {
+					WaitForSingleObject(writeOverlapped.hEvent, INFINITE);
+				}
+
+				CloseHandle(writeOverlapped.hEvent);
+
+				char buffer[1024];
+				DWORD bytesRead;
+				if (ReadFile(hPipe, buffer, sizeof(buffer), &bytesRead, nullptr) && bytesRead > 0) {
+					if (std::stoi(buffer) == 1) {
+						std::cout << "Password found for " << message << " Alphabet number: " << numberOfAlphabet << std::endl;
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+public:
+	void PipeCreate(LPWSTR pipeName, const std::string& login, int indexOfAlphabet, int passLength) {
+		auto startTime = std::chrono::high_resolution_clock::now();
+
+		HANDLE hPipe = CreateFile(
+			pipeName,
+			GENERIC_READ | GENERIC_WRITE,
+			0,
+			nullptr,
+			OPEN_EXISTING,
+			FILE_FLAG_OVERLAPPED,
+			nullptr
+		);
+
+		if (hPipe == INVALID_HANDLE_VALUE) {
+			std::cerr << "Failed to connect to the server. Error code: " << GetLastError() << std::endl;
+			return;
+		}
+
+		std::cout << "Attempting to establish a connection for " << login << " with the server.\n";
+
+		if (!BruteForce(hPipe, login, indexOfAlphabet, passLength)) {
+			std::cout << "Password not found for login " << login << std::endl;
+		}
+
+		CloseHandle(hPipe);
+
+		auto endTime = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+		std::cout << "Time taken for " << login << "  duration: " << duration << " ms" << std::endl << std::endl;
+	}
+
+	void PipeTest(LPWSTR pipeName, const std::string& login) {
+
+		std::string message;
+		std::cout << "Enter test login:password" << std::endl;
+		std::cin >> message;
+
+		auto startTime = std::chrono::high_resolution_clock::now();
+
+		HANDLE hPipe = CreateFile(
+			pipeName,
+			GENERIC_READ | GENERIC_WRITE,
+			0,
+			nullptr,
+			OPEN_EXISTING,
+			FILE_FLAG_OVERLAPPED,
+			nullptr
+		);
+
+		if (hPipe == INVALID_HANDLE_VALUE) {
+			std::cerr << "Failed to connect to the server. Error code: " << GetLastError() << std::endl;
+			return;
+		}
+
+		OVERLAPPED writeOverlapped;
+		ZeroMemory(&writeOverlapped, sizeof(OVERLAPPED));
+		writeOverlapped.hEvent = CreateEvent(nullptr, TRUE, FALSE, nullptr);
+
+		if (WriteFile(hPipe, message.c_str(), message.length() + 1, nullptr, &writeOverlapped) || GetLastError() == ERROR_IO_PENDING) {
+			WaitForSingleObject(writeOverlapped.hEvent, INFINITE);
+		}
+
+		CloseHandle(writeOverlapped.hEvent);
+
+		char buffer[1024];
+		DWORD bytesRead;
+		if (ReadFile(hPipe, buffer, sizeof(buffer), &bytesRead, nullptr) && bytesRead > 0) {
+			if (std::stoi(buffer) == 1) {
+				std::cout << "Password found: " << message << std::endl;
+			}
+		}
+
+		DisconnectNamedPipe(hPipe);
+		CloseHandle(hPipe);
+
+		auto endTime = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+		std::cout << "Time taken for " << login << "  duration: " << duration << " ms" << std::endl << std::endl;
+	}
+};
+
+int main() {
+
+	LPWSTR namePipe1 = const_cast<LPWSTR>(L"\\\\.\\pipe\\MyNamedPipe1");
+
+	PasswordCracker passwordCracker;
+
+	int choice;
+
+	while (true) {
+		std::cout << "Select the operating mode:\n";
+		std::cout << "1. Communication check\n";
+		std::cout << "2. Hacking\n";
+
+		std::cin >> choice;
+
+		if (choice == 1) {
+			auto future1 = std::async(std::launch::async, [namePipe1, &passwordCracker] {
+				passwordCracker.PipeTest(namePipe1, "login1");
+				});
+		}
+		else if (choice == 2) {
+			std::string logins[] = { "login2", "login3", "login4", "login5" };
+			int passLength = 10;
+			int numberOfAlphabet = 0;
+
+			std::cout << "Enter the maximum allowable password length: ";
+			std::cin >> passLength;
+
+			std::cout << "Alphabet 0 - small Latin letters and apostrophe\n";
+			std::cout << "Alphabet 1 - small and capital Latin letters and apostrophe + numbers\n";
+			std::cout << "Alphabet 2 - small and capital Latin letters and apostrophe + numbers + small and capital Cyrillic letters\n";
+			std::cout << "Select the alphabet number (0, 1, or 2):";
+			std::cin >> numberOfAlphabet;
+
+
+			int loginHack[4];
+
+			for (int i = 0; i < 4; i++) {
+			
+			}
+
+			std::vector<std::future<int>> futures;
+			// создание асинхронно четырех именнованых канала
+			for (int i = 0; i < 4; ++i) {
+				std::future<int> fut = std::async(std::launch::async, [i, &passwordCracker, logins, numberOfAlphabet, passLength] {
+					std::wstring pipeName = L"\\\\.\\pipe\\MyNamedPipe" + std::to_wstring(i + 2);
+					passwordCracker.PipeCreate(const_cast<LPWSTR>(pipeName.c_str()), logins[i], numberOfAlphabet, passLength);
+					return i;
+					});
+				std::this_thread::sleep_for(std::chrono::milliseconds(1)); // микрослип дл€ того, что бы текст не накладывалс€ друг на друга
+				futures.push_back(std::move(fut));
+			}
+
+			for (auto& fut : futures) {
+				fut.get();
+			}
+
+			int repeatChoice;
+			std::cout << "Should I repeat the operation again? 1 - repeat; 0 - no\n";
+			std::cin >> repeatChoice;
+
+			if (repeatChoice == 1) {
+				continue;
+			}
+			else {
+				break;
+			}
+		}
+		else {
+			std::cout << "Wrong choice. Please select 1 or 2.\n";
+			std::cin.clear();
+			while (std::cin.get() != '\n');
+		}
+	}
+
+	return 0;
+}
---
+++ server/server.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <thread>
+#include <future>
+#include <fstream>
+#include <vector>
+#include <chrono>
+
+struct UserInfo {
+    std::string login;
+    std::string password;
+};
+
+class NamedPipeServer {
+public:
+    NamedPipeServer(const std::wstring& pipeName, const std::string& login, const std::string& password)
+        : pipeName(pipeName), login(login), password(password) {}
+
+    void PipeCreate() {
+        bool shouldContinue = true;
+
+        while (shouldContinue) { // экзотический костыль для работы тестового логина
+
+            hPipe = CreateNamedPipe(
+                pipeName.c_str(),
+                PIPE_ACCESS_DUPLEX | FILE_FLAG_OVERLAPPED,
+                PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
+                PIPE_UNLIMITED_INSTANCES,
+                1024, 1024,
+                NMPWAIT_USE_DEFAULT_WAIT,
+                nullptr
+            );
+
+            if (hPipe == INVALID_HANDLE_VALUE) {
+                std::cerr << "Failed to create a named channel. Error code: " << GetLastError() << std::endl;
+                return;
+            }
+
+            OVERLAPPED overlapped;
+            ZeroMemory(&overlapped, sizeof(OVERLAPPED));
+            overlapped.hEvent = CreateEvent(nullptr, TRUE, FALSE, nullptr);
+
+            if (ConnectNamedPipe(hPipe, &overlapped) || GetLastError() == ERROR_IO_PENDING) {
+                WaitForSingleObject(overlapped.hEvent, INFINITE);
+                std::cout << "Client with login: " + login + " was connected." << std::endl;
+
+                while (WaitForSingleObject(overlapped.hEvent, 0) == WAIT_OBJECT_0) {
+                    char buffer[1024];
+                    DWORD bytesRead;
+
+                    if (ReadFile(hPipe, buffer, sizeof(buffer), &bytesRead, &overlapped) || GetLastError() == ERROR_IO_PENDING) {
+                        DWORD waitResult = WaitForSingleObject(overlapped.hEvent, INFINITE);
+                        if (waitResult == WAIT_OBJECT_0) {
+                            std::string receivedData(buffer);
+                            size_t pos = receivedData.find(':');
+                            std::string receivedLogin = receivedData.substr(0, pos);
+                            std::string receivedPassword = receivedData.substr(pos + 1);
+
+                            if (receivedLogin == login && receivedPassword == password) {
+                                const char* response = "1";
+                                WriteFile(hPipe, response, strlen(response) + 1, nullptr, nullptr);
+                                std::cout << "Client with login: " << login << " was disconnected." << std::endl;
+                                CloseHandle(hPipe);
+
+                                if (login == "login1") {
+                                    shouldContinue = true;
+                                    break;
+                                }
+                                else {
+                                    shouldContinue = false;
+                                }
+                            }
+                            else {
+                                const char* response = "0";
+                                WriteFile(hPipe, response, strlen(response) + 1, nullptr, nullptr);
+                            }
+                        }
+                    }
+                }
+            }
+            else {
+                std::cerr << "Failed to connect a named channel. Error code: " << GetLastError() << std::endl;
+            }
+
+            CloseHandle(overlapped.hEvent);
+            CloseHandle(hPipe);
+        }
+    }
+
+private:
+    std::wstring pipeName;
+    std::string login;
+    std::string password;
+    HANDLE hPipe;
+};
+
+int main() {
+
+    OPENFILENAME ofn;
+    char szFile[260];
+
+    ZeroMemory(&ofn, sizeof(ofn));
+    ofn.lStructSize = sizeof(ofn);
+    ofn.hwndOwner = NULL;
+    ofn.lpstrFile = (LPWSTR)szFile;
+    ofn.lpstrFile[0] = '\0';
+    ofn.nMaxFile = sizeof(szFile);
+    ofn.lpstrFilter = L"Text Files\0*.txt\0All Files\0*.*\0";
+    ofn.nFilterIndex = 1;
+
+    std::vector<UserInfo> users;
+
+    if (GetOpenFileName(&ofn) == TRUE) {
+        std::ifstream file(ofn.lpstrFile);
+        if (file.is_open()) {
+            std::string dummy;
+            std::getline(file, dummy);
+
+            while (!file.eof()) {
+                UserInfo user;
+                file >> user.login >> user.password;
+                users.push_back(user);
+            }
+
+            file.close();
+        }
+        else {
+            std::cerr << "Unable to open file" << std::endl;
+            return 1;
+        }
+    }
+    else {
+        std::cerr << "File selection canceled by user" << std::endl;
+        return 1;
+    }
+
+    while (1) {
+        std::future<int>* futures = new std::future<int>[users.size()];
+
+        for (int i = 0; i < users.size(); ++i) {
+            futures[i] = std::async(std::launch::async, [i, users] {
+                std::wstring pipeName = L"\\\\.\\pipe\\MyNamedPipe" + std::to_wstring(i + 1);
+                NamedPipeServer pipeServer(pipeName, users[i].login, users[i].password);
+                pipeServer.PipeCreate();
+                return i;
+            });
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        }
+
+        std::cout << "The server is waiting for connections\n";
+
+        for (int i = 1; i < users.size(); ++i) {
+            futures[i].get();
+        }
+
+        int result = MessageBox(NULL, L"Continue working?", L"Shall I continue?", MB_YESNO | MB_ICONQUESTION);
+
+        if (result != IDYES) {
+            break;
+        }
+    }
+
+    return 0;
+}
